85cb20a2fb05d72d1807b4e9bdb60322aedb2ce2
sunyta2 <sunyta2@naver.com>
2016-10-27 14:19:57 +0900

    Add files via upload

---
+++ Untitled 2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	using namespace std;
+	int intOne;
+	int &rSomeRef = intOne;
+	
+	intOne =5;
+	cout<<"intOne: "<<intOne<<endl;
+	cout<<"rSomeRef: "<<rSomeRef<<endl;
+	
+	rSomeRef=7;
+	cout<<"intOne: "<<intOne<<endl;
+	cout<<"rSomeRef: "<<rSomeRef<<endl;
+	
+	cout<<"&intOne: "<<&intOne<<endl;
+	cout<<"&rSomeRef: "<<&rSomeRef<<endl;
+	
+
+}
